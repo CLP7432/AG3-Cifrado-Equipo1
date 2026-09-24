2efb94186ae990017dc35410186fcdad62d8377d
CLP7432 <121730557+CLP7432@users.noreply.github.com>
2026-09-24 08:18:05 -0600

    Add files via upload

---
+++ AlgoritmoCifradoAsimetrico/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.ComponentModel;
+
+namespace AlgoritmoCifradoAsimetrico
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                UnicodeEncoding byteConverter = new UnicodeEncoding();
+
+                //Variable menor a 50 caracteres
+                string mensajeOriginal = "UNIDEH Hernandez Trinidad Carrasco Flores";
+
+                byte[] dataToEncrypt = byteConverter.GetBytes(mensajeOriginal);
+                byte[] encryptedData;
+                byte[] decryptedData;
+
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    encryptedData = RSAEncrypt(dataToEncrypt, RSA.ExportParameters(false), false);
+                    decryptedData = RSADecrypt(encryptedData, RSA.ExportParameters(true), false);
+                }
+                Console.WriteLine("Mensaje original:        {0}\n", mensajeOriginal);
+                Console.WriteLine("Mensaje encriptado:      {0}\n", BitConverter.ToString(encryptedData));
+                
+
+                Console.WriteLine("Mensaje desencriptado:    {0}\n", byteConverter.GetString(decryptedData));
+                Console.ReadKey();
+
+
+            }
+            catch (ArgumentNullException ex)
+            {
+
+            }
+        }
+
+        public static byte[] RSAEncrypt(byte[] dataToEncrypt, RSAParameters RSAKeyInfo, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] encryptedData;
+
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAKeyInfo);
+                    encryptedData = RSA.Encrypt(dataToEncrypt, DoOAEPPadding);
+                }
+                return encryptedData;
+            }
+            catch (CryptographicException ex)
+            {
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.ReadKey();
+                    return null;
+                }
+            }
+        }
+
+        public static byte[] RSADecrypt(byte[] DataToDecrypt, RSAParameters RSAKeyInfo, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] decryptedData;
+
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAKeyInfo);
+
+                    decryptedData = RSA.Decrypt(DataToDecrypt, DoOAEPPadding);
+                }
+                return decryptedData;
+            }
+            catch (CryptographicException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+                return null;
+            }
+        }
+    }
+}
---
+++ AlgoritmoCifradoSimetrico/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace AlgoritmoCifradoSimetrico
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string mensajeOriginal = "UNIDEH Hernandez Trinidad Carrasco Flores";
+            using (Aes myAes = Aes.Create())
+            {
+                byte[] mesajeEncryptado = EncriptarStringToBytes_Aes(mensajeOriginal, myAes.Key, myAes.IV);
+                Console.WriteLine("Mensaje original:          {0}\n", mensajeOriginal);
+                Console.WriteLine("Mensaje Encriptado:        {0}\n", BitConverter.ToString(mesajeEncryptado));
+
+                string mensajeDesencriptado = DesencriptarStringFromBytes_AES(mesajeEncryptado, myAes.Key, myAes.IV);
+
+                Console.WriteLine("Mensaje Desencriptado:      {0}\n", mensajeDesencriptado);
+
+                Console.ReadKey();
+            }
+        }
+
+        static byte[] EncriptarStringToBytes_Aes(string textoOriginal, byte[] key, byte[] IV)
+        {
+            //Definicion de argumentos del algoritmo AES
+            if(textoOriginal == null || textoOriginal.Length <= 0)
+            {
+                throw new ArgumentNullException("Textooriginal");
+            }
+            if(key  == null || key.Length <= 0)
+            {
+                throw new ArgumentNullException("key");
+            }
+            if(IV == null || IV.Length <= 0)
+            {
+                throw new ArgumentNullException("IV");
+            }
+            byte[] mensajeEncriptado = null;
+
+            using (Aes aesAlgoritmo = Aes.Create())
+            {
+                aesAlgoritmo.Key = key;
+                aesAlgoritmo.IV = IV;
+
+                ICryptoTransform encryptador = aesAlgoritmo.CreateEncryptor(aesAlgoritmo.Key, aesAlgoritmo.IV);
+
+                using (MemoryStream msEcrypt = new MemoryStream())
+                {
+                    using (CryptoStream cssEncrypt = new CryptoStream(msEcrypt, encryptador, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(cssEncrypt))
+                        {
+                            swEncrypt.Write(textoOriginal);
+                        }
+                        mensajeEncriptado = msEcrypt.ToArray();
+                    }
+                }
+            }
+            return mensajeEncriptado;
+        }
+
+        static string DesencriptarStringFromBytes_AES(byte[] textoEncryiptado, byte[] key, byte[] IV)
+        {
+            if (textoEncryiptado == null || textoEncryiptado.Length <= 0)
+            {
+                throw new ArgumentNullException("textoEncryiptado");
+            }
+            if (key == null || key.Length <= 0)
+            {
+                throw new ArgumentNullException("key");
+            }
+            if (IV == null || IV.Length <= 0)
+            {
+                throw new ArgumentNullException("IV");
+            }
+            
+            string textoDesencriptado = null;
+
+            using (Aes aesAlgiritmo = Aes.Create())
+            {
+                aesAlgiritmo.Key = key;
+                aesAlgiritmo.IV = IV;
+
+                ICryptoTransform desencriptador = aesAlgiritmo.CreateDecryptor(aesAlgiritmo.Key, aesAlgiritmo.IV);
+
+                using (MemoryStream msDecrypt = new MemoryStream(textoEncryiptado))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, desencriptador, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            textoDesencriptado = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return textoDesencriptado;
+        }
+    }
+}
